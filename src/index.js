7f7354bb70c2d10bc3a4368470194a6a58ab4832
frankynztein <arellanfranciree@gmail.com>
2019-06-14 18:19:31 -0500

    Corrección de tests

--- src/index.js
+++ src/index.js
@@ -1,58 +1,69 @@
 const path = require('path');
 const fs = require('fs');
 
-// let content = fs.readFile('Estefanía%20Telis/Documents/GitHub/', function (err, content) {
-//   console.log(content);
-//   let lines =  content.toString().split('\n').length -1;
-  
-// });
+// NODE PATH/FS
+export const fileStat = fs.stat;
+export const fileExtName = path.extname;
+export const readDir = fs.readdir;
+
 
 let ruta1 = 'C:/Users/Estefanía Telis/Documents/GitHub/LIM009-fe-md-links';
 let ruta2 = 'C:/Users/Estefanía Telis/Documents/GitHub/LIM009-Cipher/README.md';
-// let ruta3 = 'src/demo_path.js';
+let ruta3 = 'src/demo_path.js';
+let ruta4 = 'C:/Users/Estefanía Telis/Documents/Prueba';
 
-export const isPathAbsolute = (ruta) => {
-  let absolutePath = path.isAbsolute(ruta);
-  if (absolutePath === true) {
-    // console.log('es absoluto');
+export const isPathAbsolute = (route) => {
+  let absolutePath = path.isAbsolute(route);  
+  if (absolutePath) {
+    // console.log('es absoluto', absolutePath);
+    return absolutePath;
   } else {
-    // console.log('es relativo');
-    path.resolve(ruta);
-    // console.log(path.resolve(ruta));
+    // console.log('es relativo', path.resolve(route));
+    return path.resolve(route);
   }
 };
 
 isPathAbsolute(ruta1);
-isPathAbsolute(ruta2);
-// isPathAbsolute(ruta3);
+isPathAbsolute(ruta3);
 
-export const validatePath = (path) => {
-  fs.access(path, (err) => {
-    if(err) {
-      console.log('Ruta inválida.');
-    } else {
-      console.log('Ruta válida.'); 
-    }
-    // console.log(`${err ? 'does not exist' : 'exists'}`);
-  });
-};
+// export const validatePath = (path) => {
+//   fs.stat(path, (err, data) => {
+//     if(err) {
+//       console.log(err);
+//       console.log('Ruta inválida.');
+//     } else {
+//       console.log('Ruta válida.');
+//       console.log(data.isFile());
+      
+//       return data.isFile();
+//     }
+//   });
+// };
 
-// validatePath(ruta1);
-// validatePath(ruta1);
+// validatePath(ruta2);
+// console.log(validatePath(ruta2));
 
-export const isPathAFile = (path) => {
-  fs.stat(path, (err, data) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log('¿Es un archivo? ' + data.isFile());  
-    };
-  });
+// validatePath(ruta3);
+
+export const isPathAFile = (route) => {
+  return new Promise ((resolve, rejection) => {
+    fileStat(route, (err, data) => {
+      if (err) {
+        rejection(err);
+      } else {
+        resolve(data.isFile());
+      };
+    })
+  })
 };
 
 // isPathAFile(ruta1);
 // isPathAFile(ruta2);
 // isPathAFile(ruta3);
+
+// isPathAFile(ruta2).then(result => console.log(result));
+// .catch(err => console.log(err));
+
 
 export const isPathDirectory = (path) => {
   fs.stat(path, (err, data) => {
@@ -67,38 +78,79 @@ export const isPathDirectory = (path) => {
 // isPathDirectory(ruta1);
 // isPathDirectory(ruta2);
 
-export const pathExtName = (ruta) => {
-  let extName = path.extname(ruta);
+export const pathExtName = (route) => {
+  let extName = fileExtName(route);
   let arrMdFiles = [];
   if (extName === '.md') {
-    arrMdFiles.push(ruta);
+    arrMdFiles.push(route);
   }
-  console.log('arrmdfiles ' + arrMdFiles);
-  
+  // console.log('arrMdFiles= ', arrMdFiles);
+  return arrMdFiles;
 };
 
-// pathExtName(ruta1);
 // pathExtName(ruta2);
+// console.log('pathextname ', pathExtName(ruta2));
 
 // CUANDO LA RUTA ES UN ARCHIVO
 export const readDirectory = (route) => {
-  fs.readdir(route, (err, file) => {
-    if (err) {
-      console.log(err);
-    } else {
-      let directoryFiles = file;
-      // console.log(directoryFiles);
-      let arrDirectoryAbsolutePaths = []; 
-      directoryFiles.forEach((fileData) => {
-        let directoryAbsolutePath = path.join(route, fileData);
-        // console.log(directoryAbsolutePath);
-        arrDirectoryAbsolutePaths.push(directoryAbsolutePath);
-      });
-      console.log(arrDirectoryAbsolutePaths);
-
-    }
+  return new Promise((resolve, rejection) => {
+    readDir(route, (err, file) => {
+      if (err) {
+        rejection(err);
+      } else {
+        // resolve(resolveReadDirectory(file));
+        resolve(file);
+      };
+    });
   });
 };
 
-readDirectory(ruta1);
+// readDirectory(ruta1);
 // readDirectory(ruta2);
+readDirectory(ruta4).then(result => {
+  result.forEach(f =>{
+    // console.log(getMdFilesPaths(f))
+  })
+});
+
+// readDirectory(ruta4).then(result => result);
+
+// OBTENER RUTAS DE ARCHIVOS .MD EN CARPETAS (LUEGO DE readDirectory)
+export const getMdFilesPaths = (result) => {
+  let directoryFiles = result;
+  // console.log(directoryFiles);
+  let arrDirectoryAbsolutePaths = [];
+  let arrMdFilesAbsolutePaths = []; 
+  directoryFiles.forEach((fileData) => {
+    let directoryAbsolutePath = path.join(route, fileData);
+    // console.log(directoryAbsolutePath);
+    arrDirectoryAbsolutePaths.push(directoryAbsolutePath);
+  });
+  arrDirectoryAbsolutePaths.forEach((route) => {        
+    if (path.extname(route) === '.md') {
+      arrMdFilesAbsolutePaths.push(route);
+      // console.log(arrMdFilesAbsolutePaths);
+      return arrMdFilesAbsolutePaths
+    };
+  });
+};
+
+
+
+
+
+// LEYENDO ARCHIVO
+// export const readingFile = (route) => {
+//   fs.readFile(route, 'utf8', (err, data) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       // console.log(data);
+//       const regularExp = //;
+//       console.log(regularExp.exec(data));
+      
+//     }
+//   });
+// };
+
+// readingFile(ruta2);
--- test/index.spec.js
+++ test/index.spec.js
@@ -1,7 +1,31 @@
-import { isPathAbsolute } from "../lib/index.js";
+import { isPathAbsolute, isPathAFile, pathExtName } from "../src/index.js";
 
 describe ('isPathAbsolute', () => {
-  it('Debería ser una función', () => {
-    expect(typeof isPathAbsolute).toBe('function');
+  it('Debería ser true para ruta absoluta', () => {
+    expect(isPathAbsolute('C:/Users/Estefanía Telis/Documents/GitHub/LIM009-fe-md-links')).toEqual(true);
+  });
+
+  it('Debería retornar una ruta absoluta cuando el argumento es una ruta relativa', () => {
+    expect(isPathAbsolute('README.md')).toBe("C:\\Users\\Estefanía Telis\\Documents\\GitHub\\LIM009-fe-md-links\\README.md");
   });
 });
+
+describe ('isPathAFile', () => {
+  test('Debería ser true para archivo', () => {
+    return isPathAFile('C:/Users/Estefanía Telis/Documents/GitHub/LIM009-Cipher/README.md').then((result) => {
+      expect(result).toBe(true);
+    });
+  });
+
+  test('Debería ser false para carpeta', () => {
+    return isPathAFile('C:/Users/Estefanía Telis/Documents/GitHub/LIM009-Cipher').then((result) => {
+      expect(result).toBe(false);
+    });
+  });
+});
+
+describe ('pathExtName', () => {
+  it('Debería crear un array con la ruta del archivo con extensión .md', () => {
+    expect(pathExtName('C:/Users/Estefanía Telis/Documents/GitHub/LIM009-Cipher/README.md')).toEqual(['C:/Users/Estefanía Telis/Documents/GitHub/LIM009-Cipher/README.md'])
+  })
+})
